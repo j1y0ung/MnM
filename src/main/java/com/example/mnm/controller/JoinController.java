0c06616e19cb22e2cf903b2581fd8ee508dac4b4
DESKTOP-QHT7LFJ\HJY <jiyounghaan@gmail.com>
2021-06-20 04:39:28 +0900

    [UPDATE] 회원가입 검증 코드 추가, 카테고리 가져오는 코드 추가

--- src/main/java/com/example/mnm/controller/JoinController.java
+++ src/main/java/com/example/mnm/controller/JoinController.java
@@ -1,26 +1,32 @@
 package com.example.mnm.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.mnm.domain.Account;
+import com.example.mnm.domain.Category;
 import com.example.mnm.service.MnmStoreFacade;
+import com.example.mnm.validator.MemberRegisterValidator;
+
+import net.sf.json.JSONArray;
 
 @Controller
 @RequestMapping("join")
 public class JoinController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	private String formViewName = "thyme/joinForm";
+	private String formViewName = "JoinForm";
 
 	@Autowired
 	private MnmStoreFacade store;
@@ -28,34 +34,55 @@ public class JoinController {
 		this.store = store;
 	}
 	
-	// 회원가입 폼으로 이동
-	@RequestMapping(method=RequestMethod.GET)
-	public String joinForm() {
-		logger.info("joinForm()");
-		return formViewName;
-	}
-
-	@ModelAttribute
+	@ModelAttribute("account")
 	protected Object joinformBackingObject() throws Exception {
 		logger.info("joinformBackingObject()");
 		return new Account();
 	}
 	
+	// 회원가입 폼으로 이동
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView joinForm() {
+		logger.info("joinForm()");
+		ModelAndView mav = new ModelAndView();
+		// 카테고리 목록 가져오기
+		List<Category> categoryList = store.getCategoryList();
+		mav.setViewName("JoinForm");
+		mav.addObject("categoryList", JSONArray.fromObject(categoryList));
+		logger.info("mav: " + JSONArray.fromObject(categoryList));
+		return mav;
+	}
+
 	// 계정 추가 실행
 	@RequestMapping(method=RequestMethod.POST)
-    public String insertAccount(Account account, BindingResult result,
-            RedirectAttributes redirect, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		logger.info("insertAccount()");
-		logger.info("userid:" + account.getUserid());
-		logger.info("email:" + account.getEmail());
-		logger.info("name:" + account.getName());
-		logger.info("addr:" + account.getAddr());
-		logger.info("phone:" + account.getPhone());
-		logger.info("favcategory:" + account.getFavcategory());
-		logger.info("pwd:" + account.getPwd());
-		logger.info("kakao:" + account.getKakao());
+    public String insertAccount(@ModelAttribute("account") Account account, BindingResult result, HttpServletRequest request, Model model) {
 		
-        store.insertAccount(account);
-        return "redirect:/";
+		logger.info("insertAccount()");
+		new MemberRegisterValidator().validate(account, result);
+		model.addAttribute("account", account);
+		
+		if(result.hasErrors()) {
+			List<Category> categoryList = store.getCategoryList();
+			model.addAttribute("categoryList", JSONArray.fromObject(categoryList));
+            return formViewName;
+		}
+		else {
+			account.setEmail(account.getEmail() + account.getEmailDomain());
+			account.setPhone(account.getPhone() + "-" +account.getPhone2() + "-" + account.getPhone3());
+			account.setFavcategory(request.getParameter("category1"));
+			account.setKakao("n");
+			
+			logger.info("userid:" + account.getUserid());
+			logger.info("name:" + account.getName());
+			logger.info("addr:" + account.getAddr());
+			logger.info("pwd:" + account.getPwd());
+			logger.info("kakao:" + account.getKakao());
+			logger.info("email:" + account.getEmail());
+			logger.info("phone:" + account.getPhone());
+			logger.info("favcategory:" + account.getFavcategory());
+		}
+		
+		store.insertAccount(account);
+		return "redirect:/login";
     }
 }
