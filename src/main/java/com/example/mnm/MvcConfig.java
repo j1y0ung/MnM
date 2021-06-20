7810f98654d7ec20bd3d10dd41ac276dabbe911f
woni77 <onuonu2222@gmail.com>
2021-06-20 15:00:39 +0900

    변경없음

--- src/main/java/com/example/mnm/MvcConfig.java
+++ src/main/java/com/example/mnm/MvcConfig.java
@@ -1,5 +1,5 @@
 package com.example.mnm;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
